7be4b52f9db3e1e24c868a248bad0c085a8db3f5
codered-jack <sumitnbq@gmail.com>
2021-07-23 12:39:24 +0530

    created container deploy script

--- packages/container/config/webpack.common.js
+++ packages/container/config/webpack.common.js
@@ -1,17 +1,24 @@
-module.exports ={
-    module:{
-        rules:[
-            {
-                test: /\.m?js$/,
-                exclude:/node_modules/,
-                use:{
-                    loader:'babel-loader',
-                    options:{
-                        presets:['@babel/preset-react','@babel/preset-env'], //preset-react for jsx conversion and preset-env for es-2016,17,.. convertion to es2015
-                        plugins:['@babel/plugin-transform-runtime'] //enable async await syntax inside browser
-                    }
-                }
-            }
-        ]
-    }
-}
+const HTMLWebpackPlugin = require('html-webpack-plugin'); 
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react", "@babel/preset-env"], //preset-react for jsx conversion and preset-env for es-2016,17,.. convertion to es2015
+            plugins: ["@babel/plugin-transform-runtime"], //enable async await syntax inside browser
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
